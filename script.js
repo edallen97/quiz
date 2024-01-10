9dbee17ef8eb31059636c59ef5e9a826ab4b4d71
edallen97 <edward_allen@hotmail.co.uk>
2024-01-10 14:59:26 +0000

    Jake's changes

--- script.js
+++ script.js
@@ -6,110 +6,114 @@
 // Review page:
 // Score out of 10 and for extra credit list the questions the user got wrong and what the correct answer was
 
-
-// selectors
+// // selectors
 let startQuizBtn = document.querySelector('.startQuizBtn')
-let answerSelector = document.querySelector('.answerSelector')
+// let answerSelector = document.querySelector('.answerSelector')
 
 
 
 
 // event listeners
-startQuizBtn.addEventListener('click', startQuiz)
+// startQuizBtn.addEventListener('click', startQuiz)
 
 
 
+function initQuestions(){
+    // question object
+    questionObject = [
+        {
+            question: 'How old is Ed?',    
+            answers: [
+                17,
+                21,
+                26,
+                427
+            ],
+            correctAnswer : 2,
+        },
 
-// question object
-let questionObject = [
-    {question: 'How old is Ed?',
-    
-    answers: [
-        17,
-        21,
-        26,
-        427
-    ],
-    correctAnswer : 2,
-    },
+        {
+            question: 'Question 2',
+            answers: [
+                1,
+                2,
+                3,
+                4
+            ],
+            correctAnswer : 2,
+        },
 
-    {question: 'Question 2',
-    
-    answers: [
-        1,
-        2,
-        3,
-        4
-    ],
-    correctAnswer : 2,
-    },
+        {
+            question: 'Question 3',
+            answers: [
+                1,
+                2,
+                3,
+                4
+            ],
+            correctAnswer : 2,
+        },
 
-    {question: 'Question 3',
-    
-    answers: [
-        1,
-        2,
-        3,
-        4
-    ],
-    correctAnswer : 2,
-    },
+        {
+            question: 'Question 4',
+            answers: [
+                1,
+                2,
+                3,
+                4
+            ],
+            correctAnswer : 2,
+        }
+        //...
+    ]
+}
 
-    {question: 'Question 4',
-    
-    answers: [
-        1,
-        2,
-        3,
-        4
-    ],
-    correctAnswer : 2,
-    }
-    //...
-]
-
-
+function storeTimestamp(){
+    // record time when quiz was started 
+    let timestamp = new Date().getTime() // this is a unix timestamp
+    // divide by 1000 for seconds
+    console.log(timestamp)
+    return timestamp
+}
 
 // click button to start quiz
 function startQuiz() {
-    // record time when quiz was started 
-    const timestamp = new Date().getTime() // this is a unix timestamp
-    // divide by 1000 for seconds
-    console.log(timestamp)
+    startTime = storeTimestamp()
+
+    initQuestions()
     // remove startQuizBtn
     startQuizBtn.remove()
     // display 1st question
+
+    currentIndex = 0
+
     let questionDiv = document.createElement('div')
     questionDiv.setAttribute('class', 'questionDiv')
+
+
+    populateQuestion(currentIndex)
+}
+
+function populateQuestion(currentIndex, ammendedAnswer=false) {
     
-    let answerSelectorForm = document.createElement('form')
-    questionDiv.appendChild(answerSelectorForm)
-
-    let submitBtn = document.createElement('input')
-    submitBtn.setAttribute('class', 'submitBtn')
-    answerSelectorForm.appendChild(submitBtn)
-    submitBtn.addEventListener('click', () => {
-        submitAnswer()
-        nextQuestion()
-    })
     
-
-    let currentIndex = 0
-
-
-    for (let i = 1; i < 5; i++) {
+    let answers = questionObject[currentIndex].answers
+    let answerSelectorForm = document.querySelector('div.answerSelectorForm')
+    for (let i = 0; i < answers.length; i++) {
         let answerBtn = document.createElement('input')
-        let answerBtnLabel = document.createElement('label')
-        let lineBreak = document.createElement('br')
-
         answerBtn.setAttribute('id', 'answer' + [i])
         answerBtn.setAttribute('type', 'radio')
-        answerBtn.setAttribute('name', 'answer')
+        answerBtn.setAttribute('name', 'answer' + [i])
         
-        submitBtn.setAttribute('type', 'button')
-        submitBtn.innerHTML = 'Submit'
-
+        let answerBtnLabel = document.createElement('label')
         answerBtnLabel.setAttribute('for', 'answer' + [i])
+        console.log(answers[1])
+        answerBtnLabel.setAttribute('innerText', answers[i])
+
+        let lineBreak = document.createElement('br')
+
+        
+        
 
         // need to questionObject[++] on next question click
         answerBtnLabel.innerHTML = questionObject[currentIndex].answers[i]
@@ -119,36 +123,96 @@ function startQuiz() {
 
         }
 
+
+    let submitBtn = document.createElement('button')
+    submitBtn.setAttribute('class', 'submitBtn')
+
+    answerSelectorForm.appendChild(submitBtn)
+    submitBtn.addEventListener('click', () => {
+        nextQuestion(currentIndex)
+    })
+    
+    // if(ammendedAnswer){
+    //     //mark the appropriate checkbox
+    //     // show results page instead
+    //     submitBtn.addEventListener('click', () => {
+    //     finalScreen()
+    // })
+    // }
+
+   
+
+}
     
         // display next question
-    function nextQuestion() {
+function nextQuestion(currentIndex){
+    //check answer provided
+    answerProvided = checkAnswer()
+    if(answerProvided){
+        if(currentIndex <= questionObject.length){
+            //store answer
+        submitAnswer(currentIndex)
+        //remove existing question
+        removeQuestion()
+        //populate new question
         currentIndex++
-        console.log(currentIndex + ' B')
+        populateQuestion(currentIndex)
+        console.log(questionDiv)
+        } else {
+            //show our final form
+            displayResults()
+        }
+        
+    } else {
+    //if none provided:
+        alert("you must provide an answer")
     }
 
-    document.body.appendChild(questionDiv)
-    console.log(questionDiv)
-    return answerSelector
     }
 
-
-
-let answersArray = []
-
+function checkAnswer(){
+    //make this check if an answer has been selected
+    return true // or true if it has been selected
+}
+function removeQuestion(){
+    let answerSelectorForm = document.querySelector('div.answerSelectorForm')
+    answerSelectorForm.innerHTML = ""
+}
 
 // submitBtn click, add answer choice to array, display next question
-function submitAnswer() {
-    let inputs = document.querySelectorAll('.questionDiv input[type="radio"]')
+function submitAnswer(currentIndex) {
+    currentQuestionObject = questionObject[currentIndex]
+    console.log('here')
+    let inputs = document.querySelectorAll('.answerSelectorForm input[type="radio"]')
     for (let i = 0; i < inputs.length; i++){
+        console.log(i)
         if (inputs[i].checked) {
-            let checkedAnswer = inputs[i].id
-            answersArray.push(checkedAnswer)
-            console.log(answersArray)
+            currentQuestionObject.providedAnswer = i
+            console.log(currentQuestionObject)
         }
     }
 }
 
+function displayResults(){
 
+    //much harder:
+    //additional features
+    //final check screen: display all questions and your answer
+    //functionality (button) to change an existing answer then return to final check screen after
+    //call populateQuestion(index, ammendedAnswer=true)
+    //final final submit button
+
+    //loop over questionObject
+    //check providedAnswer against correctAnswer
+    //count number of correct
+    //record completed timestamp
+    //calculate how long it took
+    //display score and time taken
+
+
+    //easier:
+    //provide correct answer for each incorrect answer
+}
 
 
 // after 10 question move to a complete quiz section
